965e5243b72ea9ddbd791cdd418ea114725cb69d
Vlada Philippova <vladaphilippova6@gmail.com>
2024-10-21 23:18:42 +0500

    tests_bun

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -2,10 +2,13 @@ import pytest
 from bun import Bun
 from helpers import Burger_Info
 
-@pytest.mark.parametrize('name, price, expected_name', [
-    (Burger_Info.Bun_1, Burger_Info.Price_Bun, Burger_Info.Bun_1),
-    (Burger_Info.Bun_2, Burger_Info.Price_Bun, Burger_Info.Bun_2),
-])
-def test_bun_get_name(name, price, expected_name):
-    bun = Bun(name, price)
-    assert bun.get_name() == expected_name
+
+@pytest.mark.parametrize('name', [Burger_Info.Bun_1, Burger_Info.Bun_2])
+def test_bun_get_name(name):
+    bun = Bun(name, 0)
+    assert bun.get_name() == name
+
+
+def test_bun_get_price():
+    bun = Bun('Галактика', float(Burger_Info.Price_Bun))
+    assert bun.get_price() == float(Burger_Info.Price_Bun)
